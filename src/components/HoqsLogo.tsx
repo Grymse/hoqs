b64a91fafcc2048ccc52be3f2217be126e57414d
Nicolai Grymer <pronicoxd@gmail.com>
2024-02-04 18:39:11 +0100

    Updates

--- src/components/HoqsLogo.tsx
+++
@@ -1,9 +0,0 @@
-import logo from '../assets/logo.svg';
-
-interface HoqsLogoProps {
-  size: number;
-}
-
-export default function HoqsLogo({ size = 128 }: HoqsLogoProps) {
-  return <img src={logo} height={size} width={size} alt="HOQS logo" />;
-}
--- src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-export const supabase = createClient(
-  'https://vgtljajaxrfrybokahvz.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZndGxqYWpheHJmcnlib2thaHZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY3MTU4MTgsImV4cCI6MjAyMjI5MTgxOH0.slXBTlpcNh3XfVAjmiREdR5YvUAdq_Q9aeAapMHkaZg'
-);
