21fe5d56bf2f4f61d9f012fb9f5f2756d2bd6b17
Nicolai Grymer <pronicoxd@gmail.com>
2024-04-13 21:09:08 +0200

    Fix tailwind

--- apps/main-site/tailwind.config.js
+++ apps/main-site/tailwind.config.js
@@ -1,3 +1,4 @@
+const { nextui } = require('@nextui-org/react');
 const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
 const { join } = require('path');
 
@@ -6,12 +7,29 @@ module.exports = {
   content: [
     join(
       __dirname,
+      './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
       '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
     ),
     ...createGlobPatternsForDependencies(__dirname),
   ],
+  safelist: [
+    'bg-default-100',
+    'text-default-500',
+    'bg-primary-100',
+    'text-primary-500',
+    'bg-success-100',
+    'text-success-500',
+    'bg-warning-100',
+    'text-warning-500',
+    'bg-danger-100',
+    'text-danger-500',
+    'bg-secondary-100',
+    'text-secondary-500',
+  ],
+
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: 'class',
+  plugins: [nextui()],
 };
