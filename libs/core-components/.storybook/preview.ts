63a2ab3b793263a4f46fe6e6a247138c28c0d35c
abcdmku <63693423+abcdmku@users.noreply.github.com>
2024-04-14 11:47:15 -0500

    Add storybook

---
+++ libs/core-components/.storybook/preview.ts
@@ -0,0 +1,12 @@
+import 'apps/main-site/src/index.css';
+import { withThemeByClassName } from '@storybook/addon-themes';
+
+export const decorators = [
+  withThemeByClassName({
+    themes: {
+      light: 'light',
+      dark: 'dark',
+    },
+    defaultTheme: 'light',
+  }),
+];
